c14deb5c99e95e926eebcb26f812ca1120cd6cd4
Nguyễn Kim Vương Ý <134944912+vuongynk@users.noreply.github.com>
2025-05-22 16:48:28 +0700

    Add .next/static/chunks/src_app_courses_page_tsx_da749535._.js 1747907308313

---
+++ .next/static/chunks/src_app_courses_page_tsx_da749535._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_b814814e._.js",
+  "static/chunks/src_675f63b8._.js"
+],
+    source: "dynamic"
+});
