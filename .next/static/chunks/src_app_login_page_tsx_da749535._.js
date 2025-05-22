6ea8681ed6f4ecc4f1c0cdb94e49d4bdf7a77630
Nguyễn Kim Vương Ý <134944912+vuongynk@users.noreply.github.com>
2025-05-22 16:48:24 +0700

    Add .next/static/chunks/src_app_login_page_tsx_da749535._.js 1747907304167

---
+++ .next/static/chunks/src_app_login_page_tsx_da749535._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_fa23d011._.js",
+  "static/chunks/src_0e970483._.js"
+],
+    source: "dynamic"
+});
