85796f1010c6ebacc06a8b2c582355f9b9d8811b
Nguyễn Kim Vương Ý <134944912+vuongynk@users.noreply.github.com>
2025-05-22 16:47:21 +0700

    Add online-learning-backend/src/main/java/com/onlinelearning/dto/user/CreateInstructorRequest.java 1747907242015

---
+++ online-learning-backend/src/main/java/com/onlinelearning/dto/user/CreateInstructorRequest.java
@@ -0,0 +1,36 @@
+package com.onlinelearning.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateInstructorRequest {
+    @NotBlank(message = "First name is required")
+    private String firstName;
+    
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+    
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+    
+    @NotBlank(message = "Password is required")
+    private String password;
+    
+    @NotBlank(message = "Specialization is required")
+    private String specialization;
+    
+    @NotBlank(message = "Qualification is required")
+    private String qualification;
+    
+    private String bio;
+    private String profileImage;
+}
