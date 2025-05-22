38cd825bffd5cce3cfd2465e7c49fcb7e06de323
Nguyễn Kim Vương Ý <134944912+vuongynk@users.noreply.github.com>
2025-05-22 16:48:59 +0700

    Add .next/static/chunks/src_app_dashboard_page_tsx_a993527f._.js 1747907339841

---
+++ .next/static/chunks/src_app_dashboard_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_63654440._.js",
+  "static/chunks/src_05c0c1c3._.js"
+],
+    source: "dynamic"
+});
